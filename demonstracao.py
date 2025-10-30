d307200b2dbda49a29dfb214d6d102739fe1fa98
w1esther <w1mariaesther@gmail.com>
2025-10-30 19:06:57 -0300

    atualizações triângulos semelhantes

--- demonstracao.py
+++ demonstracao.py
@@ -3,8 +3,8 @@ from manim import *
 class Demonstracao_Pitagoras(Scene):
     def construct(self):
         
-        # malha = NumberPlane()
-        # self.play(FadeIn(malha))
+        malha = NumberPlane()
+        self.play(FadeIn(malha))
 
         A = np.array([0, 3, 0])
         B = np.array([-3, -1, 0])
@@ -16,12 +16,15 @@ class Demonstracao_Pitagoras(Scene):
         triangulo_mover = VGroup()
         triangulo_mover.add(triangulo)
 
+        ponto1 = np.array([0, -1, 0])
+
         AB = Line(A, B)
         AC = Line(A, C)
         BC = Line(B, C)
         BA = Line(B, A)
         CA = Line(C, A)
         CB = Line(C, B)
+        AH = Line(A, ponto1)
 
         triangulo_mover.add(AB, AC, BC, BA, CA, CB)
 
@@ -43,16 +46,14 @@ class Demonstracao_Pitagoras(Scene):
 
         self.play(FadeIn(ang_reto), FadeIn(beta_label), FadeIn(alfa_label), FadeIn(ang_alfa), FadeIn(ang_beta))
 
-        ponto = np.array([0, -1, 0])
-
         BC_vec = C - B
         t = np.dot(A - B, BC_vec) / np.dot(BC_vec, BC_vec)
         ponto = B + t * BC_vec
 
         altura = DashedLine(A, ponto, color=PINK, stroke_width=3, dash_length=0.2, dashed_ratio=0.5)
 
-        linha_altura = Line(ponto, A)
-        linha_BC = Line(ponto, B)
+        linha_altura = Line(ponto1, A)
+        linha_BC = Line(ponto1, B)
         linha_BC_direita = Line(ponto, C)
 
         ang_reto2 = RightAngle(linha_altura, linha_BC, length=0.4, quadrant=(1, 1), color=YELLOW)
@@ -63,19 +64,55 @@ class Demonstracao_Pitagoras(Scene):
 
         self.play(Create(altura), FadeIn(ang_reto2), FadeIn(ang_reto3))
 
-        trianguloBAH = Polygon(A, B, ponto, color=RED)
-        trianguloACH = Polygon(A, C, ponto, color=RED)
+        trianguloBAH = Polygon(A, B, ponto1, color=RED)
+        trianguloACH = Polygon(A, C, ponto1, color=RED)
 
-        self.play(Create(trianguloBAH), Create(trianguloACH))
+        trianguloACH_mover = VGroup()
+        trianguloACH_mover.add(trianguloACH)
+
+        trianguloBAH_mover = VGroup()
+        trianguloBAH_mover.add(trianguloBAH)
+
+        ang_beta_BAH = Angle(BC, BA, radius=0.6, other_angle=False, color=ORANGE)
+
+        ang_alfa_ACH = Angle(CA, CB, radius=0.6, other_angle=False, color=GREEN)
+
+        beta_label_BAH = MathTex(r"\beta").move_to(ang_beta.point_from_proportion(0.5) + 0.3*UP + 0.5*RIGHT)
+
+        alfa_label_ACH = MathTex(r"\alpha").move_to(ang_alfa.point_from_proportion(0.5) + 0.3*UP + 0.5*LEFT)
+
+        # alfa_label_BAH = MathTex(r"\alpha").move_to(ang_alfa.point_from_proportion(0.5) + 0.3*UP + 0.5*LEFT)
+
+        # beta_label_ACH = MathTex(r"\beta").move_to(ang_beta.point_from_proportion(0.5) + 0.3*UP + 0.5*RIGHT)
+
+        ang_alfa_BAH = Angle(AB, AH, radius=0.6, other_angle=False, color=GREEN)
+
+        ang_beta_ACH = Angle(AH, AC, radius=0.6, other_angle=False, color=ORANGE)
+
+        trianguloBAH_mover.add(beta_label_BAH, ang_beta_BAH, ang_alfa_BAH)
+        trianguloACH_mover.add(alfa_label_ACH, ang_alfa_ACH, ang_beta_ACH)
+
+        self.play(Create(trianguloBAH_mover), Create(trianguloACH_mover))
+
+        self.play(FadeIn(beta_label_BAH), FadeIn(ang_beta_BAH), FadeIn(ang_alfa_ACH), FadeIn(alfa_label_ACH))
 
         self.wait(2)
 
         self.play(triangulo_mover.animate.move_to(UP*2 + 3*LEFT))
         self.play(triangulo_mover.animate.scale(0.5))
 
-        self.play(trianguloBAH.animate.shift((0.5*DOWN)+(3.5*RIGHT)))
+        self.play(trianguloBAH_mover.animate.shift((0.5*DOWN)+(3.5*RIGHT)))
 
-        self.play(trianguloBAH.animate.rotate(-90*DEGREES))
+        self.play(trianguloBAH_mover.animate.rotate(-90*DEGREES))
+
+        self.wait()
+
+        self.play(trianguloBAH_mover.animate.shift(RIGHT))
+
+        self.play(trianguloBAH_mover.animate.shift(-1*RIGHT))
+        self.play(trianguloBAH_mover.animate.shift(UP))
+        self.play(trianguloBAH_mover.animate.shift(-1*UP))
 
         self.wait(2)
-    
+
+        
