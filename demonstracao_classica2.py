75c8af5f1450509e6a2d658368260920d39e9844
w1esther <w1mariaesther@gmail.com>
2025-11-20 14:40:05 -0300

    atualização

--- demonstracao_classica2.py
+++ demonstracao_classica2.py
@@ -6,16 +6,16 @@ class DemonstracaoClassicaPitagoras(MovingCameraScene):
 
         self.play(self.camera.frame.animate.scale(1.3))
 
-        malha = NumberPlane()
-        self.play(FadeIn(malha))
+        # malha = NumberPlane()
+        # self.play(FadeIn(malha))
 
         A = np.array([0, 2, 0])
         B = np.array([3, 0, 0])
         C = np.array([0, 0, 0])
 
-        triangulo1 = Polygon(A, B, C, color=BLUE_C, fill_color = BLUE_C, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
-        triangulo3 = Polygon(A, B, C, color=RED, fill_color = RED, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
-        triangulo2 = Polygon(A, B, C, color=ORANGE, fill_color = ORANGE, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
+        triangulo1 = Polygon(A, B, C, color=PINK, fill_color = PINK, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
+        triangulo3 = Polygon(A, B, C, color=PINK, fill_color = PINK, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
+        triangulo2 = Polygon(A, B, C, color=PINK, fill_color = PINK, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
         triangulo4 = Polygon(A, B, C, color=PINK, fill_color = PINK, fill_opacity = 1.0).shift(2.5*LEFT + 2.5*DOWN)
 
         label_a_t1 = MathTex('a', color=WHITE).shift(3*LEFT+1.5*DOWN)
@@ -23,10 +23,10 @@ class DemonstracaoClassicaPitagoras(MovingCameraScene):
         label_a_t3 = MathTex('a', color=WHITE).shift(3*RIGHT+1.5*UP)
         label_a_t4 = MathTex('a', color=WHITE).shift(1.5*RIGHT+3*DOWN)
 
-        label_b_t1 = MathTex("b", color=GRAY).shift(1.5*LEFT + 3*DOWN)
-        label_b_t2 = MathTex("b", color=GRAY).shift(1.5*UP + 3*LEFT)
-        label_b_t3 = MathTex("b", color=GRAY).shift(1.5*RIGHT + 3*UP)
-        label_b_t4 = MathTex("b", color=GRAY).shift(1.5*DOWN + 3*RIGHT)
+        label_b_t1 = MathTex("b", color=WHITE).shift(1.5*LEFT + 3*DOWN)
+        label_b_t2 = MathTex("b", color=WHITE).shift(1.5*UP + 3*LEFT)
+        label_b_t3 = MathTex("b", color=WHITE).shift(1.5*RIGHT + 3*UP)
+        label_b_t4 = MathTex("b", color=WHITE).shift(1.5*DOWN + 3*RIGHT)
 
         label_c_t1 = MathTex("c", color = WHITE).shift(1.2*LEFT+1*DOWN)
         label_c_t2 = MathTex("c", color = WHITE).shift(1.2*LEFT+1*UP)
@@ -34,6 +34,8 @@ class DemonstracaoClassicaPitagoras(MovingCameraScene):
         label_c_t4 = MathTex("c", color = WHITE).shift(1.2*RIGHT+1*DOWN)
 
         c_quadrado = MathTex("c^2")
+        b_quadrado = MathTex("b^2").shift(1*DOWN+1*LEFT)
+        a_quadrado = MathTex("a^2").shift(1.5*RIGHT + 1.5*UP)
 
         t1 = VGroup()
         t1.add(triangulo1, label_a_t1, label_b_t1)
@@ -49,6 +51,11 @@ class DemonstracaoClassicaPitagoras(MovingCameraScene):
         self.play(triangulo2.animate.rotate(-90*DEGREES).shift(0.5*LEFT + 2.5*UP),triangulo3.animate.rotate(-180*DEGREES).shift(3*UP + 2*RIGHT), triangulo4.animate.rotate(-270*DEGREES).shift(0.5*UP + 2.5*RIGHT))
         
         self.wait()
+
+        quadrado_inv = Square(side_length=3, stroke_width = 0, fill_opacity = 0).shift(1*LEFT + 1*DOWN)
+        quadrado_inv2 = Square(side_length=2, stroke_width = 0, fill_opacity = 0).shift(1.5*UP + 1.5*RIGHT)
+
+        self.play(FadeIn(quadrado_inv), FadeIn(quadrado_inv2))
 
         self.play(FadeIn(label_a_t2), FadeIn(label_a_t3), FadeIn(label_a_t4), FadeIn(label_a_t1))
 
@@ -66,17 +73,48 @@ class DemonstracaoClassicaPitagoras(MovingCameraScene):
 
         self.play(FadeIn(label_c_t1), FadeIn(label_c_t2), FadeIn(label_c_t3), FadeIn(label_c_t4))
 
+        mult = MathTex(r"\cdot").shift(0.2*LEFT)
+
         self.wait()
 
-        self.play(Transform(label_c_t1, c_quadrado), Transform(label_c_t2, c_quadrado), Transform(label_c_t3, c_quadrado), Transform(label_c_t4, c_quadrado))
+        self.play(label_c_t1.animate.shift(1*UP+0.5*RIGHT), label_c_t2.animate.shift(1*DOWN+1.5*RIGHT), FadeOut(label_c_t3), FadeOut(label_c_t4), FadeIn(mult))
 
-        self.play(FadeOut(label_c_t1), FadeOut(label_c_t2), FadeOut(label_c_t3), FadeOut(label_c_t4))
+        self.play(Transform(label_c_t1, c_quadrado), Transform(label_c_t2, c_quadrado), FadeOut(mult))
 
-        self.play(self.camera.frame.animate.shift(2*RIGHT), quadrado.animate.shift(6*RIGHT), c_quadrado.animate.shift(6*RIGHT))
+        self.wait()
+
+        self.play(self.camera.frame.animate.shift(2*RIGHT), quadrado.animate.shift(6*RIGHT), c_quadrado.animate.shift(6*RIGHT), FadeOut(label_c_t1), FadeOut(label_c_t2))
 
         self.wait()
 
         self.play(t3.animate.shift(1.95*LEFT), t2.animate.shift(3.0*RIGHT + 2.0*DOWN), t1.animate.shift(3*UP))
+
+        self.wait()
+
+        self.play(quadrado_inv.animate.set_fill(color=YELLOW_E, opacity=1))
+
+        mult2 = MathTex(r"\cdot").shift(1*LEFT+1*DOWN)
+
+        self.play(label_b_t1.animate.shift(1*DOWN), label_b_t2.animate.shift(0.5*DOWN + 0.5*LEFT), FadeIn(mult2))
+
+        self.play(Transform(label_b_t1, b_quadrado), Transform(label_b_t2, b_quadrado), FadeOut(mult2))
+
+        self.play(quadrado_inv2.animate.set_fill(color=BLUE_E, opacity=1))
+
+        mult3 = MathTex(r"\cdot").shift(1.5*RIGHT+1.5*UP)
+
+        self.play(label_a_t2.animate.shift(0.5*UP + 0.5*RIGHT), FadeIn(mult3))
+
+        self.play(Transform(label_a_t2, a_quadrado), Transform(label_a_t3, a_quadrado), FadeOut(mult3))
+
+        self.play(self.camera.frame.animate.scale(1.5).shift(2*RIGHT + 2* DOWN))
+
+        self.play(quadrado.animate.shift(3*RIGHT+6*DOWN), c_quadrado.animate.shift(3*RIGHT + 6*DOWN), quadrado_inv.animate.shift(5*DOWN +1*LEFT), label_b_t1.animate.shift(5*DOWN + 1*LEFT), FadeOut(label_b_t2), quadrado_inv2.animate.shift(7.5*DOWN + 1.5*RIGHT), label_a_t2.animate.shift(7.5*DOWN +1.5*RIGHT), FadeOut(label_a_t3))
+
+        mais = MathTex("+").shift(0.8*RIGHT + 6*DOWN)
+        igual = MathTex("=").shift(6*DOWN + 5.5*RIGHT)
+
+        self.play(FadeIn(mais), FadeIn(igual))
 
         # self.play(quadrado.animate.shift(5*RIGHT))
 
